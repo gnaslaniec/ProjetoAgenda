7c6ad7270e060aafe164f5294dcbb6bb25ce325e
Gabriel J <gabrieljn95@hotmail.com>
2019-09-19 11:39:31 -0300

    Criado CRUD para telefone

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_modus import Modus
-from config import Config
 
 import os
 
@@ -50,9 +49,10 @@ class Telefone(db.Model):
     categoria = db.Column(db.Text)
     contato_id = db.Column(db.Integer, db.ForeignKey('contatos.id'))
 
-    def __init__(self, numero_telefone, categoria):
+    def __init__(self, numero_telefone, categoria, contato_id):
         self.numero_telefone = numero_telefone
         self.categoria = categoria
+        self.contato_id = contato_id
 
 
 @app.route('/')
@@ -126,6 +126,39 @@ def enderecos_exibir(contato_id,id):
         return redirect(url_for('enderecos_index', contato_id=endereco_encontrado.contato_id))
     return render_template('enderecos/exibir.html', endereco=endereco_encontrado)
 
+''' Rotas Telefone '''
+@app.route('/contatos/<int:contato_id>/telefones', methods=["GET","POST"])
+def telefones_index(contato_id):
+    if request.method == 'POST':
+        new_telefone = Telefone(request.form['numero_telefone'], request.form['categoria'], contato_id)
+        db.session.add(new_telefone)
+        db.session.commit()
+        return redirect(url_for('telefones_index', contato_id=contato_id))
+    return render_template('telefones/index.html', contato=Contato.query.get(contato_id))
+
+@app.route('/contatos/<int:contato_id>/telefones/novo', methods=["GET","POST"])
+def telefones_novo(contato_id):
+    return render_template('telefones/novo.html', contato=Contato.query.get(contato_id))
+
+@app.route('/contatos/<int:contato_id>/telefones/<int:id>/editar')
+def telefones_editar(contato_id,id):
+    telefone_editar = Telefone.query.get(id)
+    return render_template('telefones/editar.html', telefone=telefone_editar)
+
+@app.route('/contatos/<int:contato_id>/telefones/<int:id>', methods=["GET","PATCH","DELETE"])
+def telefones_exibir(contato_id,id):
+    telefone_encontrado = Telefone.query.get(id)
+    if request.method == b"PATCH":
+        telefone_encontrado.numero_telefone = request.form['numero_telefone']
+        telefone_encontrado.categoria = request.form['categoria']
+        db.session.add(telefone_encontrado)
+        db.session.commit()
+        return redirect(url_for('telefones_index', contato_id=telefone_encontrado.contato_id))
+    if request.method == b"DELETE":
+        db.session.delete(telefone_encontrado)
+        db.session.commit()
+        return redirect(url_for('telefones_index', contato_id=telefone_encontrado.contato_id))
+    return render_template('telefones/exibir.html', endereco=telefone_encontrado)
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
